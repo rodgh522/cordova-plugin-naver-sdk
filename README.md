08dfc5e8e33a0d4ac2ad2c0c5e33e935219403aa
rodgh522 <rodgh522@gmail.com>
2021-01-28 10:06:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,98 @@
+# naver-sdk
+Naver Cordova SDK Plugin Wrapper (네이버 계정 연동 플러그인 Wrapper)
+
+## Development Environment and ETC
+|type|version
+|---|---
+|ionic (Ionic CLI)|5.4.16
+|cordova (Cordova CLI)|9.0.0
+|Cordova Platforms Android|8.1.0
+|Cordova Platforms IOS|5.1.1
+|NaverThirdPartyLogin.framework(ios)|4.1.2
+|naveridlogin-android-sdk(android)|4.2.6
+
+## How to install
+install cordova plugin
+```
+// OAUTH_CLIENT_ID: client id that you got assigned from naver development application you created
+// OAUTH_CLIENT_SECRET: client secret that you got assigned from naver development application you created
+// OAUTH_CLIENT_NAME: naver development application name
+// OAUTH_CLIENT_APP_SCHEME: app scheme that you registered in naver development application for ios
+$ cordova plugin add cordova-plugin-naver-sdk --variable OAUTH_CLIENT_ID=YOUR_CLIENT_ID --variable OAUTH_CLIENT_SECRET=YOUR_CLIENT_SECRET --variable OAUTH_CLIENT_NAME=YOUR_CLIENT_NAME --variable OAUTH_CLIENT_APP_SCHEME=YOUR_CLIENT_APP_SCHEME
+```
+
+install wrapper for naver cordova sdk plugin to interface
+```
+$ npm install --save naver-sdk
+```
+
+then import __NaverCordovaSDK__ module into app.module.ts
+```
+import { NaverCordovaSDK } from 'naver-sdk';
+
+@NgModule({
+  providers: [
+    NaverCordovaSDK
+  ]
+})
+```
+
+## Methods
+### `login()`
+If Naver app is installed in the device, will open the app and the login will be proceeded through the app, and return the values that are related to the token info and the user profile info, otherwise in case the app is not installed, just an webview will be popped up to sign in.
+```
+  constructor(public _naverCordovaSDK: NaverCordovaSDK) {
+    this._naverCordovaSDK.login().then((res) => {
+        console.log(res);
+      }
+    );
+  }
+```
+beside token info(accessToken, refreshToken, expiresAt, and tokenType), other return values depend on what you set on the naver development console.
+
+### `logout()`
+```
+  constructor(public _naverCordovaSDK: NaverCordovaSDK) {
+    this._naverCordovaSDK.logout().then(() => {
+        //do your logout proccess for your app
+      }
+    );
+  }
+```
+return null
+
+### `unlinkApp()`
+Unregister app for your app service. 
+```
+  constructor(public _naverCordovaSDK: NaverCordovaSDK) {
+    this._naverCordovaSDK.unlinkApp().then(() => {
+        //do your unregister proccess for your app
+      }
+    );
+  }
+```
+
+### `refreshAccessToken()`
+Refresh access token if you need.
+```
+  constructor(public _naverCordovaSDK: NaverCordovaSDK) {
+    this._naverCordovaSDK.refreshAccessToken().then((res) => {
+        console.log(res);
+      }
+    );
+  }
+```
+it returns a new access token.
+
+### `getAccessToken()`
+Get current access token.
+```
+  constructor(public _naverCordovaSDK: NaverCordovaSDK) {
+    this._naverCordovaSDK.getAccessToken().then((res) => {
+        console.log(res);
+      }
+    );
+  }
+```
+it returns the current access token.
+
---
+++ src/ios/NaverCordovaSDK.m
@@ -0,0 +1,256 @@
+#import "NaverCordovaSDK.h"
+#import <Cordova/CDVPlugin.h>
+#import <NaverThirdPartyLogin/NaverThirdPartyLogin.h>
+#import <objc/runtime.h>
+
+@interface NaverCordovaSDK ()
+
+@property(strong, nonatomic) NSString *loginCallbackId;
+@end
+
+@implementation NaverCordovaSDK
+
+- (void)pluginInitialize {
+    NSLog(@"Start Naver plugin");
+
+    // Delegate 설정
+    [NaverThirdPartyLoginConnection getSharedInstance].delegate = self;
+
+    // 네이버 앱과, 인앱 브라우저 인증을 둘다 사용하도록 설정
+    [[NaverThirdPartyLoginConnection getSharedInstance] setIsNaverAppOauthEnable:YES];
+    [[NaverThirdPartyLoginConnection getSharedInstance] setIsInAppOauthEnable:YES];
+
+    // 세로 화면 고정 설정
+    [[NaverThirdPartyLoginConnection getSharedInstance] setOnlyPortraitSupportInIphone:YES];
+
+    // 네이버 플러그인 데이터 설정
+    NSString *serviceUrlScheme = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NaverAppScheme"];
+    NSString *consumerKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NaverClientID"];
+    NSString *consumerSecret = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NaverClientSecret"];
+    NSString *appName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NaverClientName"];
+
+    [[NaverThirdPartyLoginConnection getSharedInstance] setServiceUrlScheme:serviceUrlScheme];
+    [[NaverThirdPartyLoginConnection getSharedInstance] setConsumerKey:consumerKey];
+    [[NaverThirdPartyLoginConnection getSharedInstance] setConsumerSecret:consumerSecret];
+    [[NaverThirdPartyLoginConnection getSharedInstance] setAppName:appName];
+    
+    
+
+}
+
+
+#pragma mark - Cordova commands
+
+/**
+ * 네이버 로그인을 요청합니다
+ *
+ * @param command
+ */
+- (void)login:(CDVInvokedUrlCommand *)command {
+
+    // 로그인 콜백 아이디 설정
+    self.loginCallbackId = command.callbackId;
+
+    // 로그인 요청
+    NaverThirdPartyLoginConnection *login = [NaverThirdPartyLoginConnection getSharedInstance];
+    [login requestThirdPartyLogin];
+}
+
+/**
+ * 토큰을 지워 로그아웃 처리 합니다.
+ *
+ * @param command
+ */
+- (void)logout:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
+    [[NaverThirdPartyLoginConnection getSharedInstance] resetToken];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+ /**
+  * 토큰을 지우고, 계정 연동을 해제합니다.
+  *
+  * @param command
+  */
+ - (void)unlinkApp:(CDVInvokedUrlCommand *)command {
+     // 콜백 아이디 설정
+     self.loginCallbackId = command.callbackId;
+
+     // 로그아웃 요청
+     NaverThirdPartyLoginConnection *loginConnection = [NaverThirdPartyLoginConnection getSharedInstance];
+     [loginConnection requestDeleteToken];
+ }
+
+
+
+#pragma mark - NaverThirdPartyLoginConnectionDelegate
+
+- (void)oauth20ConnectionDidFinishRequestACTokenWithAuthCode {
+    NSLog(@"oauth20ConnectionDidFinishRequestACTokenWithAuthCode");
+    NSString *accessToken = [[NaverThirdPartyLoginConnection getSharedInstance] accessToken];
+    NSString *refreshToken = [[NaverThirdPartyLoginConnection getSharedInstance] refreshToken];
+    NSDate *expiresAt = [[NaverThirdPartyLoginConnection getSharedInstance] accessTokenExpireDate];
+    NSString *tokenType = [[NaverThirdPartyLoginConnection getSharedInstance] tokenType];
+    NSMutableDictionary *userSession =  [NSMutableDictionary new];
+    
+    NSDictionary *result = @{
+                             @"accessToken" : accessToken,
+                             @"refreshToken" : refreshToken,
+                             @"expiresAt" : [NSString stringWithFormat:@"%f", [expiresAt timeIntervalSince1970]],
+                             @"tokenType" : tokenType
+                             };
+    [userSession addEntriesFromDictionary: result];
+    
+    NSString *urlString = @"https://openapi.naver.com/v1/nid/me"; // 사용자 프로필 호출 API URL
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    NSString *authValue = [NSString stringWithFormat:@"Bearer %@", accessToken];
+    NSString *contentType = @"text/json;charset=utf-8";
+    [urlRequest setValue:authValue forHTTPHeaderField:@"Authorization"];
+    [urlRequest setValue:contentType forHTTPHeaderField:@"Content-Type"];
+    
+    [[[NSURLSession sharedSession] dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSError *serializationError;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&serializationError];
+        
+        [userSession addEntriesFromDictionary: [json objectForKey:@"response"]];
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userSession];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+        self.loginCallbackId = nil;
+    }] resume];
+}
+
+- (void)oauth20ConnectionDidFinishRequestACTokenWithRefreshToken {
+    NSLog(@"oauth20ConnectionDidFinishRequestACTokenWithRefreshToken");
+    NSString *accessToken = [[NaverThirdPartyLoginConnection getSharedInstance] accessToken];
+    NSString *refreshToken = [[NaverThirdPartyLoginConnection getSharedInstance] refreshToken];
+    NSDate *expiresAt = [[NaverThirdPartyLoginConnection getSharedInstance] accessTokenExpireDate];
+    NSString *tokenType = [[NaverThirdPartyLoginConnection getSharedInstance] tokenType];
+
+    NSMutableDictionary *userSession =  [NSMutableDictionary new];
+    
+    NSDictionary *result = @{
+                             @"accessToken" : accessToken,
+                             @"refreshToken" : refreshToken,
+                             @"expiresAt" : [NSString stringWithFormat:@"%f", [expiresAt timeIntervalSince1970]],
+                             @"tokenType" : tokenType
+                             };
+    [userSession addEntriesFromDictionary: result];
+  
+    NSString *urlString = @"https://openapi.naver.com/v1/nid/me"; // 사용자 프로필 호출 API URL
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    NSString *authValue = [NSString stringWithFormat:@"Bearer %@", accessToken];
+    NSString *contentType = @"text/json;charset=utf-8";
+    [urlRequest setValue:authValue forHTTPHeaderField:@"Authorization"];
+    [urlRequest setValue:contentType forHTTPHeaderField:@"Content-Type"];
+    
+    [[[NSURLSession sharedSession] dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSError *serializationError;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&serializationError];
+        
+        [userSession addEntriesFromDictionary: [json objectForKey:@"response"]];
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userSession];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+        self.loginCallbackId = nil;
+    }] resume];
+}
+
+- (void)oauth20ConnectionDidFinishDeleteToken {
+    NSLog(@"oauth20ConnectionDidFinishDeleteToken");
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+
+    self.loginCallbackId = nil;
+}
+
+- (void)oauth20Connection:(NaverThirdPartyLoginConnection *)oauthConnection didFailWithError:(NSError *)error {
+    NSLog(@"oauth20Connection");
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.description];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+
+    self.loginCallbackId = nil;
+}
+
+- (void)oauth20Connection:(NaverThirdPartyLoginConnection *)oauthConnection didFailAuthorizationWithRecieveType:(THIRDPARTYLOGIN_RECEIVE_TYPE)recieveType
+{
+    NSLog(@"NaverApp login fail handler");
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"%u", (THIRDPARTYLOGIN_RECEIVE_TYPE)recieveType]];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+
+    self.loginCallbackId = nil;
+
+}
+
+- (void)oauth20Connection:(NaverThirdPartyLoginConnection *)oauthConnection didFinishAuthorizationWithResult:(THIRDPARTYLOGIN_RECEIVE_TYPE)recieveType
+{
+    NSLog(@"Getting auth code from NaverApp success!");
+}
+
+
+
+
+@end
+
+
+
+
+#pragma mark - AppDelegate Overrides
+
+@implementation AppDelegate (NaverCordovaSDK)
+
+void NMethodSwizzle(Class c, SEL originalSelector) {
+    NSString *selectorString = NSStringFromSelector(originalSelector);
+    SEL newSelector = NSSelectorFromString([@"swizzled_naver_" stringByAppendingString:selectorString]);
+    SEL noopSelector = NSSelectorFromString([@"noop_naver_" stringByAppendingString:selectorString]);
+    Method originalMethod, newMethod, noop;
+    originalMethod = class_getInstanceMethod(c, originalSelector);
+    newMethod = class_getInstanceMethod(c, newSelector);
+    noop = class_getInstanceMethod(c, noopSelector);
+    if (class_addMethod(c, originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod))) {
+        class_replaceMethod(c, newSelector, method_getImplementation(originalMethod) ?: method_getImplementation(noop), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, newMethod);
+    }
+}
+
++ (void)load
+{
+    NMethodSwizzle([self class], @selector(application:openURL:sourceApplication:annotation:));
+}
+
+// This method is a duplicate of the other openURL method below, except using the newer iOS (9) API.
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    if (!url) {
+        return NO;
+    }
+    [[NaverThirdPartyLoginConnection getSharedInstance] application:application openURL:url sourceApplication:[options valueForKey:@"UIApplicationOpenURLOptionsSourceApplicationKey"] annotation:0x0];
+    
+    NSLog(@"Naver(ori) handle url: %@", url);
+
+
+    // Call existing method
+    return [self swizzled_naver_application:application openURL:url sourceApplication:[options valueForKey:@"UIApplicationOpenURLOptionsSourceApplicationKey"] annotation:0x0];
+}
+
+- (BOOL)noop_naver_application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return NO;
+}
+
+- (BOOL)swizzled_naver_application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    if (!url) {
+        return NO;
+    }
+    
+    
+    [[NaverThirdPartyLoginConnection getSharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+
+
+    if ([url isKindOfClass:NSURL.class] && [sourceApplication isKindOfClass:NSString.class] && annotation) {
+    }
+    NSLog(@"Naver(swizzle) handle url: %@", url);
+    
+    // Call existing method
+    return [self swizzled_naver_application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+}
+@end
